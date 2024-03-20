d112c36aab5760b868568c53327594c005819be9
Vitor Machado dos Santos <104211114+machado-vitor-santos@users.noreply.github.com>
2024-03-20 19:54:11 -0300

    Reoganizar diretórios
    Reoganizei diretórios e inseri das aulas 4 á 8.

---
+++ Curso-JavaScript/Aula 5/aula.js
@@ -0,0 +1,16 @@
+const valorIngressoAdulto = 20;
+// valorIngressoAdulto = 30; -> da erro
+
+/* A variável const não pode ter seu valor redefinido 
+mudado, ao contrário da variável let
+Ex: (é uma boa prática que se use o "const" como valor de 
+variáveis e só use o "let" em caso de necessidade de reatri-
+buir os valores da variável)
+
+let valorIngressoAdulto = 20;
+valorIngressoAdulto = 30; -> Reatribui o valor (altera
+o valor da variável)
+*/
+
+console.log(valorIngressoAdulto);
+
