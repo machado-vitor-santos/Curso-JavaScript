ee428f7bea98421f616ab3c44f443ab919243fa5
Vitor Machado dos Santos <104211114+machado-vitor-santos@users.noreply.github.com>
2024-03-20 17:48:15 -0300

    Organizando Arquivos
    Aula 3 Criada

---
+++ Curso-JavaScript/Aula 3/aula.js
@@ -0,0 +1,27 @@
+// let idade = 5;
+// console.log(idade)
+// let altura = 180;
+// console.log(altura)
+
+/* "let" é o tipo da variável "idade" é o nome da variável "="
+corresponde a recebe e ";" finaliza os comandos em Java-Script
+
+Das variáveis:
+-não devem começar com números 
+- não conter espaços
+- nomes de variáveis são sensíveis a maiúsculas ou minúsculas
+
+Siga o modelo: camelCase -> boas pratica, iniciar nome com 
+minúsculas e ao escrever um nome composto introduzir maiusculas
+
+Ex: let contandoDinheiro = 55;
+
+É possivel agrupar variáveis, porém (não é uma boa prática)
+Ex: let nome,idade,altura;
+*/
+let altura = 180;
+let idade = 15;
+let cpf = "105.140.130.17";
+console.log(altura)
+console.log(idade)
+console.log(cpf)
