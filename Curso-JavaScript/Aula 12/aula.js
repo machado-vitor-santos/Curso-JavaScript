95ff6d9908dd1e2f3085b5479debd66643d23308
Vitor Machado dos Santos <104211114+machado-vitor-santos@users.noreply.github.com>
2024-03-21 12:00:58 -0300

    Operador ternário
    Operadores ternários: <, >, ?, <=, >=.
    // "?" -> caso contrário

--- Curso-JavaScript/Aula 12/aula.js
+++ Curso-JavaScript/Aula 12/aula.js
@@ -1,23 +1,9 @@
-// Operadores Aritméticos(matemáticos):
-// Operadores de Atribuição
-// Operadores de Comparação
-// Operadores Lógicos
-// Operadores Bitwise
+// Operador ternário:
+// <, >, ?, <=, >=.
+// "?" -> caso contrário
 
-// Operadores aritméticos:
-// let salario = 100;
-// +, -, *, /, **
-
-// console.log(salario + salario);
-// console.log(salario - salario);
-// console.log(salario * salario);
-// console.log(salario / salario);
-// console.log(5 ** 5);
-
-// ++ -- // Operadores de decremento e incremento
-// let idade = 18;
-// console.log(++idade); // incrementa 1
-// console.log(idade);
-
-let idade = 18;
-console.log(--idade); // decrementa 1
+/* Tem um cliente, acima de 100 pontos classe (premium), abai-
+xo de 100 pontos classe comum*/
+let pontos = 100;
+let tipo = pontos >= 100 ? 'premium' : 'comum';
+console.log(tipo)
