e417e9493752e15c3e19d90000169ca844b452eb
Vitor Machado dos Santos <104211114+machado-vitor-santos@users.noreply.github.com>
2024-03-21 08:49:26 -0300

    Finalizada Aula 7 Function
    let planoDeFundo = "imagem1";
    
    function alterarPlanoDeFundo(imagemDeFundo, transição) {
        planoDeFundo = imagemDeFundo + " " + transição;
    };
    
    console.log(planoDeFundo);
    
    alterarPlanoDeFundo("imagem2", "lenta");
    
    console.log(planoDeFundo)

--- Curso-JavaScript/Aula 8/aula.js
+++ Curso-JavaScript/Aula 8/aula.js
@@ -1,16 +1,54 @@
-const valorIngressoAdulto = 20;
-// valorIngressoAdulto = 30; -> da erro
+//Funções
+// Faz parte da base de todo software, porque ela vai controlar
+// o fluxo de execução. Entrada e saida de dados no Java Script 
+// Vai precisa de uma função.
 
-/* A variável const não pode ter seu valor redefinido 
-mudado, ao contrário da variável let
-Ex: (é uma boa prática que se use o "const" como valor de 
-variáveis e só use o "let" em caso de necessidade de reatri-
-buir os valores da variável)
+// Como declarar uma função:
+// Verbo + Substantivo
+//Ex:
+// resetaCor 
+// tranformarObjeto
+// alterarTamanho
 
-let valorIngressoAdulto = 20;
-valorIngressoAdulto = 30; -> Reatribui o valor (altera
-o valor da variável)
-*/
+let corSite = "azul";
 
-console.log(valorIngressoAdulto);
+function resetaCor(cor, tonalidade) { //"Parâmetros" resetaCor("cor"){};
+    corSite = cor + " " + tonalidade; /*Argumentos corSite = "cor"; que signi-
+     ficam o que eu vou fazer com os parâmetros que fora pa-
+     ssados em cima, tal como no exemplo (cor).
+
+     O símbolo de + irá concatenar o texto nos argumentos
+
+     + " " + melhor prática para concatenar os argumentos
+     do que a alternativa em por resetaCor("verde"," claro")
+     ênfase no espaça dado antes da string " claro".
+     */
+
+};
+
+console.log(corSite)
+resetaCor("verde", "claro");
+console.log(corSite)
+
+// console.log(corSite);
+// resetaCor();
+// console.log(corSite)
+
+let planoDeFundo = "imagem1";
+
+function alterarPlanoDeFundo(imagemDeFundo, transição) {
+    planoDeFundo = imagemDeFundo + " " + transição;
+};
+
+console.log(planoDeFundo);
+
+alterarPlanoDeFundo("imagem2", "lenta");
+
+console.log(planoDeFundo)
+
+
+
+
+
+
 
