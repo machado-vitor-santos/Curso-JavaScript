f760ef76e37a05e3cd5520e4d9209ade0d1a8420
Vitor Machado dos Santos <104211114+machado-vitor-santos@users.noreply.github.com>
2024-03-21 09:46:00 -0300

    Organização diretórios
    Próximas aulas sobre operadores
    com seus diretórios criados.

---
+++ Curso-JavaScript/Aula 13/aula.js
@@ -0,0 +1,23 @@
+// Operadores Aritméticos(matemáticos):
+// Operadores de Atribuição
+// Operadores de Comparação
+// Operadores Lógicos
+// Operadores Bitwise
+
+// Operadores aritméticos:
+// let salario = 100;
+// +, -, *, /, **
+
+// console.log(salario + salario);
+// console.log(salario - salario);
+// console.log(salario * salario);
+// console.log(salario / salario);
+// console.log(5 ** 5);
+
+// ++ -- // Operadores de decremento e incremento
+// let idade = 18;
+// console.log(++idade); // incrementa 1
+// console.log(idade);
+
+let idade = 18;
+console.log(--idade); // decrementa 1
