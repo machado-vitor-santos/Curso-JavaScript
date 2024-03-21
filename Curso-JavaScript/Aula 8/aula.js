8a3750ef5dda1471dd850c5c2853791efe3fc67b
Vitor Machado dos Santos <104211114+machado-vitor-santos@users.noreply.github.com>
2024-03-21 09:04:05 -0300

    Pausa
    Averiguar Itens Aula 7

--- Curso-JavaScript/Aula 8/aula.js
+++ Curso-JavaScript/Aula 8/aula.js
@@ -1,51 +1,13 @@
-//Funções
-// Faz parte da base de todo software, porque ela vai controlar
-// o fluxo de execução. Entrada e saida de dados no Java Script 
-// Vai precisa de uma função.
-
-// Como declarar uma função:
-// Verbo + Substantivo
-//Ex:
-// resetaCor 
-// tranformarObjeto
-// alterarTamanho
-
-let corSite = "azul";
-
-function resetaCor(cor, tonalidade) { //"Parâmetros" resetaCor("cor"){};
-    corSite = cor + " " + tonalidade; /*Argumentos corSite = "cor"; que signi-
-     ficam o que eu vou fazer com os parâmetros que fora pa-
-     ssados em cima, tal como no exemplo (cor).
-
-     O símbolo de + irá concatenar o texto nos argumentos
-
-     + " " + melhor prática para concatenar os argumentos
-     do que a alternativa em por resetaCor("verde"," claro")
-     ênfase no espaça dado antes da string " claro".
-     */
-
+// Realiza uma tarefa e não devolve nada
+function dizerNome(){
+    console.log("Vitor")
 };
 
-console.log(corSite)
-resetaCor("verde", "claro");
-console.log(corSite)
+dizerNome();
 
-// console.log(corSite);
-// resetaCor();
-// console.log(corSite)
+function multiplicarPorDois(){
 
-let planoDeFundo = "imagem1";
-
-function alterarPlanoDeFundo(imagemDeFundo, transição) {
-    planoDeFundo = imagemDeFundo + " " + transição;
 };
-
-console.log(planoDeFundo);
-
-alterarPlanoDeFundo("imagem2", "lenta");
-
-console.log(planoDeFundo)
-
 
 
 
