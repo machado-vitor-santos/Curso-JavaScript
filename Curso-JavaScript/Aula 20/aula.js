947bc44d3d5298a9ec6213d1ec59fbcb05ce324d
Vitor Machado dos Santos <104211114+machado-vitor-santos@users.noreply.github.com>
2024-03-22 11:30:17 -0300

    Aula 20 Laço for in
    consultas futuras

--- Curso-JavaScript/Aula 20/aula.js
+++ Curso-JavaScript/Aula 20/aula.js
@@ -1,2 +1,24 @@
-console.log("Vitor")
-console.log("Bem-Vindo ao Curso")
+// Laço for..in
+
+/*
+Um dos outros dos doi loops que fora feitos para iterar sobre 
+as propriedades de um objeto ou elementos de um array.
+*/ 
+
+const pessoa = {
+    nome: 'Vitor',
+    idade: 25
+};
+
+//key-value (chave e valor) ex: no atributo do objeto "nome"
+// corresponde a "chave" e value corresponde a "Vitor".
+for(let chave in pessoa) {
+    console.log(chave, pessoa['nome']); // outra forma de buscar dentro do atributo pessoa o item é pessoa['nome'], útil para quando não se sabe o nome de uma propriedade caso array
+}
+
+const cores = ['Vermelho', 'Azul', 'Verde'];
+
+// em arrays os itens são acessados através do índice
+for(let indice in cores) {
+    console.log(indice,cores[indice])
+}
