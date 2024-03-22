e65d9a36871442caa59573cbbd77f56696066927
Vitor Machado dos Santos <104211114+machado-vitor-santos@users.noreply.github.com>
2024-03-22 08:48:23 -0300

    Aula 17 Laço Loop /for

--- Curso-JavaScript/Aula 17/aula.js
+++ Curso-JavaScript/Aula 17/aula.js
@@ -1,2 +1,20 @@
-console.log("Vitor")
-console.log("Bem-Vindo ao Curso")
+//Laço/Loop For:
+
+console.log('Estou aprendendo"');
+console.log('Estou aprendendo"');
+console.log('Estou aprendendo"');
+console.log('Estou aprendendo"');
+console.log('Estou aprendendo"');
+
+// 1. For
+// for(let i = 1; i <=5; ++i){ -> variável "i" é uma abreviação de "index" ou índice
+//     console.log('Estou aprendendo', i);
+// } 
+
+
+for (let i = 1; i <= 5; ++i) {
+    if (i % 2 !== 0) { // onde % é (resto da divisão) e !== (diferente) -> mais esclarecimentos em operador "modulos"
+        console.log(i);
+    }
+}
+
