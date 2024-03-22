5d458f3b63217de91dd3acaf189541e7403d8a31
Vitor Machado dos Santos <104211114+machado-vitor-santos@users.noreply.github.com>
2024-03-22 07:18:59 -0300

    Progresso Aula 14 Else If
    problemas com vs code a arrumar

--- Curso-JavaScript/Aula 15/aula.js
+++ Curso-JavaScript/Aula 15/aula.js
@@ -1,2 +1,19 @@
-console.log("Vitor")
-console.log("Bem-Vindo ao Curso")
+// If..Else
+
+
+// Se a hora estiver entre 06:00 até 12:00 : Bom dia!
+// Se estiver entre 12:00 até as 18:00 : Boa Tarde!
+// Caso contrário : Boa Noite!
+
+let hora = 10;
+if (hora > 6 && < 12) {
+    console.log("Bom dia!"); // a ser executado
+};
+
+else if (hora > 12 && < 18) {
+    console.log("Boa Tarde!"); // outra condição
+};
+
+else {
+    console.log("Boa noite!"); //código a ser executado
+};
