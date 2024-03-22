169352bb2d0a46dce4787abdc52458f3777e449e
Vitor Machado dos Santos <104211114+machado-vitor-santos@users.noreply.github.com>
2024-03-22 08:03:50 -0300

    Aula 16 Switch Case
    switch (variável) {
    case 'parâmetro':
    escrever o comando;
    break;
    
    default (caso nenhuma condição seja satisfeita)
    }

--- Curso-JavaScript/Aula 16/aula.js
+++ Curso-JavaScript/Aula 16/aula.js
@@ -1,2 +1,20 @@
-console.log("Vitor")
-console.log("Bem-Vindo ao Curso")
+// Switch Case (outra das formas de fazer comparações dentro do JS)
+let permissao; // usuário comum, gerente, diretor
+permissao = 'diretor';
+
+switch (permissao) { // (variável que vai ativar a condição)
+    case 'comum': //tratar os diversos cenários usando o case":"
+    console.log('Usuário Comum'); // condição que será ativada com o case
+    break; // para que não continue lendo e comparando as outra condições, pois ele já encontrou a condição
+
+    case 'gerente':
+    console.log('Usuário Gerente');
+    break;
+
+    case 'diretor':
+    console.log('Usuário Diretor');
+    break;
+
+    default: // caso não caia em nenhuma das comparações
+    console.log('Usuário Não Reconhecido!');
+}
