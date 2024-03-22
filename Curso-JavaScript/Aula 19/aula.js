6ca32f7cc61f97510f66e24285fe0acb25d15b93
Vitor Machado dos Santos <104211114+machado-vitor-santos@users.noreply.github.com>
2024-03-22 09:58:47 -0300

    Aula 19 Laço/do while
    let i = x;
    do {
      (adicionar comando)
    } while (condição)

--- Curso-JavaScript/Aula 19/aula.js
+++ Curso-JavaScript/Aula 19/aula.js
@@ -1,1 +1,7 @@
 // Laço Do.. While
+
+let i = 0; // variável index tem que ser declarada fora
+do { // faça {} while (condição)
+    console.log('digitando', i);
+    i++; // obs. deve ser "++1" dentro do comando do
+} while (i < 10) // enquanto (condição)
