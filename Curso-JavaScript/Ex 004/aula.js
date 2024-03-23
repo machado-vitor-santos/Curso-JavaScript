0abbd890d5fb9169693e2cdc56cf32d1d28c649b
Vitor Machado dos Santos <104211114+machado-vitor-santos@users.noreply.github.com>
2024-03-23 07:48:00 -0300

    Adaptação ex004
    Ênfase no refatoramento dos números ao calcular, para facilitar a posterior troca dos fatores sem a necessidade de ficar trocando todos os valores correspondentes no código

--- Curso-JavaScript/Ex 004/aula.js
+++ Curso-JavaScript/Ex 004/aula.js
@@ -5,14 +5,17 @@
 // Caso os pontos sejam maior que 12 => 'carteira suspensa
 // Math.floor(1.3) = 1; => arredonda para o inteiro mais próximo
 
-let testeVelocidade = verificarVelocidade(210);
+let testeVelocidade = verificarVelocidade(98.7);
 
 function verificarVelocidade(velocidade){
-    if (velocidade <= 70)
+    const velocidadeMaxima = 70; // Criar uma const para 'refatorar', ou seja, caso seja necessário posteriormente mexer no código, não será necessário ir buscar o valor 70 em cada operação que foi empregue
+    const kmPorPonto = 5;
+    const maxPontosPermitidos = 12;
+    if (velocidade <= velocidadeMaxima)
         console.log("ok");
     else { // é possível um aninhamento de um if dentro de um else
-        const pontos = ((velocidade - 70) / 5); // a variável pontos antes do if, facilita em muito criar as condições para a limpeza do código sem necessidade de tantas restrições matemáticas
-        if (pontos > 12)
+        const pontos = ((velocidade - velocidadeMaxima) / kmPorPonto); // a variável pontos antes do if, facilita em muito criar as condições para a limpeza do código sem necessidade de tantas restrições matemáticas
+        if (pontos > maxPontosPermitidos)
         console.log('carteira suspensa');
         else {
             console.log('Pontos: ',Math.floor(pontos));
