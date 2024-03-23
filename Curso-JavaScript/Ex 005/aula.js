47770cbe1523a5952bef4d1ce83bcfb238e867aa
Vitor Machado dos Santos <104211114+machado-vitor-santos@users.noreply.github.com>
2024-03-23 12:09:25 -0300

    Ex005 conte par ou ímpar
    Utilize funções e laços de repetição para contar 5 números e categorizá-los em par ou ímpar.

--- Curso-JavaScript/Ex 005/aula.js
+++ Curso-JavaScript/Ex 005/aula.js
@@ -1,1 +1,11 @@
+// Receber uma quantidade de valores para avaliar
+// Função exibe se cada valor é impar ou par
+
+console.log(exibirTipo(5));
+
+function exibirTipo(limite) {
+    for (i = 1; i <= limite; ++i) {
+        console.log(i, (i % 2 === 0) ? 'par' : 'impar');
+    }
+};
 
