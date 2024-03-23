01171a223aea2a94c808c24ebbbcc64b217b3bad
Vitor Machado dos Santos <104211114+machado-vitor-santos@users.noreply.github.com>
2024-03-23 06:21:15 -0300

    Ex 003
    fizzBuzz

--- Curso-JavaScript/Ex 003/aula.js
+++ Curso-JavaScript/Ex 003/aula.js
@@ -1,14 +1,23 @@
 // fizzbuzz 
-// Divisível por 3 => Fizz
+// Divisível por 3 => fizz
 // Divisível por 5 => Buzz
-// Divisível por 3 e 5 => FfizzBuzz 
+// Divisível por 3 e 5 => fizzBuzz 
 // Não divisível por 3 ou 5 => entrada
 // Não é um número => 'Não é um número'
 
-const resultado = fizzBuzz(15);
+const resultado = fizzBuzz(3);
 console.log(resultado);
 
-function fizzBuzz(entrada){
-    if (entrada % 3 == 0) 
+function fizzBuzz(entrada) {
+    if (typeof entrada !== 'number')
+        return 'Não é um número';
+    if (entrada % 3 !== 0 && entrada % 5 !== 0)
+        return entrada;
+    if (entrada % 3 === 0 && entrada % 5 == 0)
+        return 'fizzBuzz';
+    if (entrada % 5 === 0)
+        return 'Buzz';
+    if (entrada % 3 === 0)
+        return 'fizz';
+};
 
-    };
