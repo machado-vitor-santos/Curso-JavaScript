1f54ab35ac3b759f483a81a60bd42218ac5312d4
Vitor Machado dos Santos <104211114+machado-vitor-santos@users.noreply.github.com>
2024-03-25 12:10:17 -0300

    Ex010 encontre n° primos

--- Curso-JavaScript/Ex 010/aula.js
+++ Curso-JavaScript/Ex 010/aula.js
@@ -1,20 +1,23 @@
-let a = 'vermelho';
-let b = 'azul';
+// números primos
+// Criar função para mostrar números primos:
 
-// function trocarValores(cor1, cor2){
-//     a = cor1;
-//     b = cor2;
-// };
+exibirNumerosPrimos(1000);
 
-// trocarValores("Azul","Vermelho")
-// console.log(a);
-// console.log(b);
+function exibirNumerosPrimos(limite) {
+    for (let i = 1; i <= limite; ++i) {
+        if (i % i === 0
+            && i % 2 !== 0
+            && i % 3 !== 0
+            && i % 5 !== 0
+            && i % 7 !== 0
+            && i ** (1 / 2) !== i)
 
-let c = a;
-a = b;
-b = c;
-
-console.log(a);
-console.log(b);
-
+            console.log(i);
+    }
+    console.log(1);
+    console.log(2);
+    console.log(3);
+    console.log(5);
+    console.log(7);
+};
 
