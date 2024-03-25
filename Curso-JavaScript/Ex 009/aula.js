f5638441e3746765e3877baad6df6dfc01700c01
Vitor Machado dos Santos <104211114+machado-vitor-santos@users.noreply.github.com>
2024-03-25 10:05:51 -0300

    comentários
    adicionado comentários ao ex 009

--- Curso-JavaScript/Ex 009/aula.js
+++ Curso-JavaScript/Ex 009/aula.js
@@ -7,6 +7,7 @@ exibirAsteriscos(10);
 
 function exibirAsteriscos(linhas) {
     for (let i = 1; i <= linhas; ++i) {
-        console.log("*".repeat(i));
+        console.log("*".repeat(i)); //O método repeat() constrói e retorna uma nova string com um determinado número de cópias concatenadas da string original.
+
     }
 };
