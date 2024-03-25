f2fe226f1f5a28b03b8304c08884eade8c631dbe
Vitor Machado dos Santos <104211114+machado-vitor-santos@users.noreply.github.com>
2024-03-25 19:02:11 -0300

    Método Math parâmetros matemáticos
    segue o link pormenorizado do MDN em português

--- Curso-JavaScript/Aula 26/aula.js
+++ Curso-JavaScript/Aula 26/aula.js
@@ -1,2 +1,13 @@
-console.log("Vitor")
-console.log("Bem-Vindo ao Curso")
+// Math
+
+//Math.random https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+Math.random() // Gera um número aleatório no console do chrome
+
+Math.max(3,6,8,10) // vai retornar o maior valor
+
+Math.min(5,8,4,9) // vai retornar o menor valor
+
+// restante da biblioteca Math em português:
+//https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math
+
+Math.pow(x,y) // Retorna a base x elevada a potência y do expoente.
