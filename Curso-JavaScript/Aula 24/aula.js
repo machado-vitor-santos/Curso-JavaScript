f3a2868e9e0069016733fa4c92243b0e5518355a
Vitor Machado dos Santos <104211114+machado-vitor-santos@users.noreply.github.com>
2024-03-25 17:52:06 -0300

    Natureza din. dos objetos como criar e deletar aatributos
    nomeobjeto.atributonovo:valor;
    nomeobjeto.nomefuncao = function(){
    comando da função
    };

--- Curso-JavaScript/Aula 24/aula.js
+++ Curso-JavaScript/Aula 24/aula.js
@@ -1,2 +1,14 @@
-console.log("Vitor")
-console.log("Bem-Vindo ao Curso")
+// Natureza Dinâmica de Objetos
+const mouse = {
+    cor: 'red',
+    marca: 'dazz'
+}
+
+mouse.velocidade = 5000; // criua um atributo dentro do objeto
+mouse.trocarDPI = function () { // insere uma função dentro do objeto
+    console.log('mudando DPI');
+};
+
+delete mouse.velocidade; // deleta um atributo dentro do objeto
+delete mouse.trocarDPI; // deleta uma função dentro do objeto
+console.log(mouse);
