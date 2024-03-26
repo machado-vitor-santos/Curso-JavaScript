6de9099152f69a4ffc9846fdfe7308000abcd77d
Vitor Machado dos Santos <104211114+machado-vitor-santos@users.noreply.github.com>
2024-03-26 20:59:36 -0300

    Adicionando itens no array
    array.unshift(); -> primeiro
    array.splice(2,0,'adendo'); -> meio
    array.push(); -> último

--- Curso-JavaScript/Aula 31/aula.js
+++ Curso-JavaScript/Aula 31/aula.js
@@ -1,2 +1,23 @@
-console.log("Vitor")
-console.log("Bem-Vindo ao Curso")
+// Aula de introdução a arrays
+// Add novos elementos
+
+const numeros = [1,2,3];
+
+//inicio
+numeros.unshift(0);
+console.log(numeros);
+
+//meio 
+numeros.splice(1,0,'a');
+console.log(numeros);
+
+//final
+numeros.push(5);
+console.log(numeros);
+
+
+const nomes = ['Vitor', 'Matheus', 'Carlos'];
+nomes.unshift('Jacapira'); //adiciona ao primeiro
+nomes.splice(2,0,'Jubirubiru'); // adicionar ao indice desejado, o do meio exclui e o teceiro dpeois da virgula escreve a variável
+nomes.push('Panamaua'); // escreve por último
+console.log(nomes);
