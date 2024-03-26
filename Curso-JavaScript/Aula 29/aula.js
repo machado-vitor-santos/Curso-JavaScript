cc8824c1ce11736cc87176dcdbabf839a3b4fe7f
Vitor Machado dos Santos <104211114+machado-vitor-santos@users.noreply.github.com>
2024-03-25 21:54:59 -0300

    Aula 29 Date
    const data1 = new Date(); -> retorna data em tempo real

---
+++ Curso-JavaScript/Aula 29/aula.js
@@ -0,0 +1,19 @@
+// Date
+
+const data1 = new Date(); // trará data atual e horário em tempo real;
+const data2 = new DataTransfer('March 06 2019 09:20'); // Um tipo de formato de data
+const data3 = new Date(2024,03,06,09,30); // Outra forma de inserir data
+console.log(data3);
+
+data3.setFullYear(2030); // modifica a const data3
+
+// conversão de objetos 'data' para 'string':
+
+data3.toTimeString(); // Informação de tempo local
+console.log(data3);
+
+date2.toISOString(); // forma que se vê a informaçãode data no banco de dados.
+
+
+
+
