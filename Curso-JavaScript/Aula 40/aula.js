540644650d5c65c2b602c60a199d13f14d73ef51
Vitor Machado dos Santos <104211114+machado-vitor-santos@users.noreply.github.com>
2024-03-27 14:21:43 -0300

    Recebendo Dados (INPUT)
    prompt(``) -> recebe dados
    alert(``) -> envia dados para tela

--- Curso-JavaScript/Aula 40/aula.js
+++ Curso-JavaScript/Aula 40/aula.js
@@ -1,2 +1,39 @@
-console.log("Vitor")
-console.log("Bem-Vindo ao Curso")
+// Combinando Arrays
+
+/*
+const numeros = [1,2,3,4,5];
+
+const combinado = numeros.join('.'); // Adiciona um . entre cada um dos valores do array
+console.log(combinado);
+
+const frase = 'Olá bem vindo ao curso'; 
+const separandoStrings = frase.split(' '); // Secciona frases por critério definido
+console.log(separandoStrings);
+
+const resultado = separandoStrings.join('-');
+console.log(resultado);
+*/
+
+//Exercício
+
+const frase = 'conteúdos gratuitos';
+console.log(frase);
+//const split = frase.join('.'); // Não funciona, pois os separadores têm de estar dentro de um Array.
+//console.log(split);
+const quebrandoFrase = frase.split(' ');
+console.log(quebrandoFrase);
+const pontosInseridos = quebrandoFrase.join('-');
+console.log(pontosInseridos);
+
+// quebrandoFrase = frase.split('.');
+// console.log(quebrandoFrase);
+
+
+
+
+
+
+
+
+
+
--- Curso-JavaScript/Aula 41/aula.js
+++ Curso-JavaScript/Aula 41/aula.js
@@ -1,2 +1,20 @@
-console.log("Vitor")
-console.log("Bem-Vindo ao Curso")
+//Recebendo Dados (INPUT)
+
+/*
+let nome = prompt('Qual é o seu nome?') // Abre um caixa de pergunta no navegador
+alert(`Seja Bem-vindo(a) ${nome} !`); // Retorna uma mensagem na caixa de pergunta do navegador
+*/
+
+// Exercicio
+// receber Idade da pessoa;
+// definir se a pessoa é maior ou menor de idade.
+
+let idade = prompt(`Qual é sua idade?`) // Recebe dados
+if (idade < 18) {
+    alert(`Você tem ${idade} anos, menor de idade.`); // Envia dados
+} else {
+    alert(`Você tem ${idade} anos, maior de idade.`);
+}
+
+
+
