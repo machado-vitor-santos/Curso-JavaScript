590e296cb60ceeceff06b4df41eb22d947f1ca09
Vitor Machado dos Santos <104211114+machado-vitor-santos@users.noreply.github.com>
2024-03-27 08:43:13 -0300

    Como consultar tipos primitivos dentro de um Array
    nomearray.indexOf()
    
    nomearray.lastIndexOf();
    
    nomearray.includes();

--- Curso-JavaScript/Aula 32/aula.js
+++ Curso-JavaScript/Aula 32/aula.js
@@ -1,2 +1,20 @@
-console.log("Vitor")
-console.log("Bem-Vindo ao Curso")
+// Java Script Aula 54, encontrando elementos (primitivos)
+
+/*
+const numeros = [1,2,3,4,1,5];
+console.log(numeros.indexOf(3));// Vai buscar o número 2 dentro do array e vai retornar o índice do número caso ele exista. Caso não encontre irá 'retornar -1'.
+// o indexOf(); filtra por tipo também , strings não se confundirão com number, booleans etc...
+
+
+console.log(numeros.lastIndexOf(1)); // vai buscar a ultima ocorre^ncia do valor procurado dentro do array
+
+console.log(numeros.indexOf(2) !== -1); // retorna se o número está contidodentro do array
+
+console.log(numeros.includes(2)); // mais simples, retorna se o número está contido ou não
+*/
+
+const nomesNumeros = ['Tevez', 'Carlos',3,2,5,20,5];
+
+console.log(nomesNumeros.indexOf('Tevez'));
+console.log(nomesNumeros.lastIndexOf(5));
+console.log(nomesNumeros.includes('Carlos'));
