85019ea9a3fe10e08a96bf558d578be416c1f32e
Vitor Machado dos Santos <104211114+machado-vitor-santos@users.noreply.github.com>
2024-03-27 10:34:52 -0300

    Combinado e Dividindo Arrays
    nomearray1.concat(nomearray2)
    // combina ambos arrays
    
    nomedocombinado.slice(a,b);
    // recorta array de a até b

--- Curso-JavaScript/Aula 37/aula.js
+++ Curso-JavaScript/Aula 37/aula.js
@@ -1,2 +1,23 @@
-console.log("Vitor")
-console.log("Bem-Vindo ao Curso")
+//Combinando ou e Dividir arrays
+
+const primeiro = [1,2,3];
+const segundo = [4,5,6];
+
+// Combina arrays
+const combinado = primeiro.concat(segundo); 
+console.log(combinado);
+
+// Divide arrays
+const cortado = combinado.slice(1,3); // (a,b) recorta do a ao b, sendo que vai ao índice b - 1
+console.log(cortado);
+
+const letras1 = ['a', 'b', 'c'];
+const letras2 = ['d', 'e', 'f'];
+
+const combinados = letras1.concat(letras2);
+console.log(combinados);
+
+const recorte = combinados.slice(1,4);
+console.log(recorte);
+
+
