f53622f32f57c805fdbc024d229c34a654622714
Vitor Machado dos Santos <104211114+machado-vitor-santos@users.noreply.github.com>
2024-03-27 09:13:30 -0300

    Encontrando elementos do tipo referência em Arrays
    let a = nomearray.find(function(){
       return if a === 'identificação';
    });
    console.log(a);

--- Curso-JavaScript/Aula 33/aula.js
+++ Curso-JavaScript/Aula 33/aula.js
@@ -1,2 +1,26 @@
-console.log("Vitor")
-console.log("Bem-Vindo ao Curso")
+// Encontrando elementos do tipo referência
+
+const marcar = [
+    {id: 1, nome: 'a'},
+    {id: 1, nome: 'b'},
+];
+
+//console.log(marcar.includes({d: 1, nome: 'a'})); // em objetos não consiguiriamos usar o inclides, como neste caso.
+
+let marca = marcar.find(function(marca){ // pricurar sobre aplicação do elemento .find() no MDN em caso de dúvida.
+    return marca.nome === 'a'; // como pegar chaves dentro de um array
+});
+
+console.log(marca);
+
+const comentarios = [
+    {id:1, mensagens:'a'},
+    {id:2, mensagens:'b'},
+    {id:3, mensagens:'c'}
+];
+
+let coments = comentarios.find(function(coments){
+    return coments.id === 3;
+});
+
+console.log(coments);
