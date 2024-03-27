5ec3f1713643c011bbdb6571671275dfdd9e9c80
Vitor Machado dos Santos <104211114+machado-vitor-santos@users.noreply.github.com>
2024-03-27 15:01:35 -0300

    Manipulando a DOM
    getElementByid();
    getElementByClass();

--- Curso-JavaScript/Aula 42/aula.js
+++ Curso-JavaScript/Aula 42/aula.js
@@ -1,2 +1,13 @@
-console.log("Vitor")
-console.log("Bem-Vindo ao Curso")
+// Intro Manipulação do DOM
+
+// DOM é basicamento todo HTML que está dentro de uma página
+
+// seleciona o elemento pelo ID
+document.getElementByid("maia-signature").innerText = `Olá meu nome é jfniwjehrijwernwjernkwejrnwer`;
+
+// seleciona o elemento pela classe
+// sempre que se trabalha com uma lista ou com um array é 
+// necessário acessar por índice.
+documento.getElementsByClass('nomeDoElemento')[0].innerText = `Chubilabilau é 10`;
+
+
