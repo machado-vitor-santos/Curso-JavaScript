00c64da0455d6e04a026916e9df2d0879ff00a62
Vitor Machado dos Santos <104211114+machado-vitor-santos@users.noreply.github.com>
2024-03-27 11:10:44 -0300

    Operador spread concatenar e clonar arrays
    let var = [...array1,...array2]; - concatena
    
    let clonado = [...array1]; - clona

--- Curso-JavaScript/Aula 38/aula.js
+++ Curso-JavaScript/Aula 38/aula.js
@@ -1,2 +1,21 @@
-console.log("Vitor")
-console.log("Bem-Vindo ao Curso")
+//Operador Spread
+
+const primeiro = [1,2,3];
+const segundo = [4,5,6];
+
+//const combinado = primeiro.concat(segundo);
+//const cortado = combinado.slice(1,3); 
+
+// Spread
+const combinado = [...primeiro,...segundo]; // Outra forma de combinar arrays
+//console.log(combinado);
+
+const clonado = [...combinado]; //Clonar um array
+//console.log(combinado);
+
+//Exercicio
+const exercicio1 = [1,2,3];
+const exercicio2 = [4,5,6];
+
+const combinando = [...exercicio1, '%' ,...exercicio2];
+console.log(combinando);
