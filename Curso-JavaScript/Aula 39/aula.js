b89068d42466d2500bc972603c4a49630c36f2b7
Vitor Machado dos Santos <104211114+machado-vitor-santos@users.noreply.github.com>
2024-03-27 11:22:43 -0300

    forEach - forma fácil de iterar um Array
    nomearray.forEach(nomes, indice =>
       console.log(nomes, indice));

--- Curso-JavaScript/Aula 39/aula.js
+++ Curso-JavaScript/Aula 39/aula.js
@@ -1,2 +1,13 @@
-console.log("Vitor")
-console.log("Bem-Vindo ao Curso")
+//FOREACH - Iterando um array
+
+const numeros = [1, 2, 3, 4, 5];
+//console.log(numeros);
+
+numeros.forEach((numero,indice) => console.log(numero,indice)); // Arrow function
+
+// Exercício
+const nomes = ['Vitor', 'Maria', 'Jonathan', 'Catia'];
+nomes.forEach((nome, indice) => console.log(nome, indice));
+
+
+
