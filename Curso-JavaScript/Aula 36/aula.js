cccce6ab926a97504ebee5bc952dd088c0e3ac4e
Vitor Machado dos Santos <104211114+machado-vitor-santos@users.noreply.github.com>
2024-03-27 10:20:54 -0300

    Como esvaziar um array
    nomearray.length = 0;

--- Curso-JavaScript/Aula 36/aula.js
+++ Curso-JavaScript/Aula 36/aula.js
@@ -1,2 +1,25 @@
-console.log("Vitor")
-console.log("Bem-Vindo ao Curso")
+//Esvaziando um Array
+
+let numeros = [1, 2, 3, 4, 5, 6];
+console.log(numeros);
+//Solução 1
+numeros = [];
+
+//Solução 2 -> apaga todas as referências do array [MELHOR METODO!]
+//numeros.length = 0;
+//console.log(numeros);
+
+//Solução 3
+numeros.splice(0, numeros.length);
+console.log(numeros);
+
+//Solução 4
+// while (numeros.lengt > 0)
+//     numeros.pop();
+
+
+let numerosLetras = [3, 'cat', 4, 'rato', 7, 'Mosca'];
+console.log(numerosLetras);
+
+numerosLetras.length = 0;
+console.log(numerosLetras);
