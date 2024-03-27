5fae6b117cae836c471ef6cc42c190b843960dcc
Vitor Machado dos Santos <104211114+machado-vitor-santos@users.noreply.github.com>
2024-03-27 09:33:59 -0300

    Uso de Arrow functions para consultas em um Array de objetos

--- Curso-JavaScript/Aula 34/aula.js
+++ Curso-JavaScript/Aula 34/aula.js
@@ -1,2 +1,21 @@
-console.log("Vitor")
-console.log("Bem-Vindo ao Curso")
+//Arrow Functions
+
+const marcar = [
+    {id:1, nome:'a'},
+    {id:2, nome:'b'}
+];
+
+console.log(marcar.find(marca => marca.id === 2)); 
+// forma simplificada de consultar um array
+// Empregue a função arrow =>
+
+const objetos = [
+    {nome:'prego',quantidade: 100},
+    {nome:'madeiras',quantidade: 200},
+    {nome:'casa',quantidade: 300}
+];
+
+console.log(objetos.find(objeto => objeto.nome === 'prego'));
+
+
+
